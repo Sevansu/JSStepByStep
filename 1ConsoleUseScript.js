a64c30b48faf88478c0823283a1d1cc5796676d8
Sevansu <sevansupatel2122@gmail.com>
2021-08-10 10:39:22 +0530

    added js file to demonstrate  use of  console

---
+++ 1ConsoleUseScript.js
@@ -0,0 +1,11 @@
+// USe ctrl+shift+i to view console in chrome
+console.log("%cLogs in Green","color: green; font-size: 0.66rem;");
+console.log("%cLogs in : %cGreen %cRed","color: #ff5500; font-size: 1rem","color: green; font-size: 1rem;","color: red; font-size: 1rem;");
+
+console.info("Information");
+
+console.debug("Debug");
+
+console.warn("warns");
+
+console.error("errors");
