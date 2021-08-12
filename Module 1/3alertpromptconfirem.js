83c67d8b4422f720d00026307ecb70151ca9fd59
Sevansu <sevansupatel2122@gmail.com>
2021-08-12 11:46:56 +0530

    crated alert,datatypes,type conversion

---
+++ Module 1/3alertpromptconfirem.js
@@ -0,0 +1,14 @@
+let mesg = "Hi, let JavaScript";
+alert( mesg );
+
+var msge = "Hi var JS";
+alert(msge);
+
+const pi = Math.PI;
+alert(`${pi} constant`);
+
+let age = prompt('How old are you?', "Enter your age");
+alert(`You are ${age} years old!`);
+
+let isStud = confirm("Are you the student?");
+alert( isStud )
---
+++ Module 1/4typeconversion.js
@@ -0,0 +1,18 @@
+//string conversion
+let val = true;
+console.log(typeof val);
+value = String(val);
+console.log(typeof value);
+
+//numeric conversion
+let str = "877";
+console.log(typeof str);
+let num = Number(str);
+console.log(typeof num);
+
+//boolean (logical)
+console.log( Boolean(11) );
+console.log( Boolean(0) );
+
+console.log( Boolean("some") );
+console.log( Boolean("") );
