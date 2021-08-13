4a7e0679988579688d6678b73d2f55739dc7d2b7
Sevansu <sevansupatel2122@gmail.com>
2021-08-13 11:16:35 +0530

    Updated

---
+++ Module 1/06operators.js
@@ -0,0 +1,12 @@
+let zz=0;
+zz++;
+console.log(zz); //--> Outputs 1
+
+let xz=0;
+++xz;
+console.log(xz); //--> Outputs 1
+
+let z=0;
+console.log(z++); //---> Outputs 0
+
+console.log(++z); //--> Outputs 2
